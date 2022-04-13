1bbd760dfcb8d0a0ce0e55d1b0216c9bd5f385ac
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-04-13 21:50:31 +0200

    added restarting and winning and losing conditions

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let getData = () => [
   { imgSrc: './images/fkatwigs.jpeg', id: 3, name: 'fka twigs' },
   { imgSrc: './images/fleetwood.jpeg', id: 4, name: 'fleetwood' },
   { imgSrc: './images/joy-division.jpeg', id: 5, name: 'joy division' },
-  { imgSrc: './images/ledzep.jpeg', id: 6, name: 'lep zeppelin' },
+  { imgSrc: './images/ledzep.jpeg', id: 6, name: 'led zeppelin' },
   { imgSrc: './images/metallica.jpeg', id: 7, name: 'metallica' },
   { imgSrc: './images/pinkfloyd.jpeg', id: 8, name: 'pink floyd' },
   { imgSrc: './images/beatles.jpeg', id: 9, name: 'beatles' },
@@ -68,6 +68,7 @@ const checkCards = (e) => {
   const clickedCard = e.target;
   clickedCard.classList.add('flipped');
   const flippedCards = document.querySelectorAll('.flipped');
+  const toggleCard = document.querySelectorAll('.toggleCard');
 
   if (flippedCards.length === 2) {
     if (
@@ -87,8 +88,41 @@ const checkCards = (e) => {
       });
       playerLives--;
       playerLivesCount.textContent = playerLives;
+
+      if (playerLives === 0) {
+        setTimeout(() => {
+          restart('try again');
+        }, 1000);
+      }
     }
+  }
+  if (toggleCard.length === 16) {
+    restart('you won');
   }
 };
 
+// restart
+
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll('.face');
+  let cards = document.querySelectorAll('.card');
+  section.style.pointerEvents = 'none';
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove('toggleCard');
+
+    // randomize
+    setTimeout(() => {
+      cards[index].style.pointerEvents = 'all';
+      faces[index].src = item.imgSrc;
+      cards[index].setAttribute('name', item.name);
+      section.style.pointerEvents = 'all';
+    }, 1000);
+  });
+  playerLives = 6;
+  playerLivesCount.textContent = playerLives;
+  setTimeout(() => {
+    window.alert(text);
+  }, 100);
+};
 cardGenerator();
